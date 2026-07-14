e0bdbca78f2d5c148842999fe8228f2567ed7c9a
unknown <lovelyldh@naver.com>
2026-07-14 09:15:46 +0900

    tokentest

--- 388352.py
+++ 388352.py
@@ -2,18 +2,36 @@ from itertools import combinations
 
 def solution(n, q, ans):
     # combination의 재료가 될 것을 생성해야 한다.
+    cnt = 0
+    m = len(q)
     tmp_set_add = {}
-    tmp_set_dicard = {}
-    for i in range(len(q)) :
-        if 
-    first_arr = 
+    tmp_set_discard = {}
+    for i in range(m) :
+        if ans[i] == 0 :
+            tmp_set_discard.update(q[i])
+        else :
+            tmp_set_add.update(q[i])
 
-    answer = 0
+    # tmp_set_discard를 이용해서 확실하게 아닌것들 먼저 쳐내고
+    for i in tmp_set_discard :
+        q.remove(i)
+
+    for i in set(combinations(q, 5)) :
+        flag = True
+        for j in range(m) :
+            if q[j]&i == ans[j] :
+                break
+        if(flag) :
+            cnt += 1
+
+    # 그런데 tmp_set_add에 한번도 안들어갔다고 해서 무조건 아니라고 할 수 있나?
+
+    answer = cnt
     return answer
 
 
 
 if __name__ == "__main__" :
-    solution(10, [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [3, 7, 8, 9, 10], [2, 5, 7, 9, 10], [3, 4, 5, 6, 7]], [2, 3, 4, 3, 3])
+    print(solution(10, [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [3, 7, 8, 9, 10], [2, 5, 7, 9, 10], [3, 4, 5, 6, 7]], [2, 3, 4, 3, 3]))
 
     # 10, [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [3, 7, 8, 9, 10], [2, 5, 7, 9, 10], [3, 4, 5, 6, 7]], [2, 3, 4, 3, 3]
--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-from bisect import * 
+from functools import wraps
+import time
 
-a = [0, 1, 2]
-b = a
-c = a[:]
-a[1] = 10
+def check_time(func) :
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        
+        result = func(*args, **kwargs)
 
-print(f"a : {a}")
-print(f"b : {b}")
-print(f"c : {c}")
+        end = time.time()
+
+        print(f"[{func.__name__}] 소요 시간 : {end - start : .2f}초")
+        return result
+    return wrapper
+
+@check_time
+def process_data_1():
+    time.sleep(1)
+
+@check_time
+def process_data_2():
+    time.sleep(2)
+
+print(process_data_1.__name__)
+process_data_2()    
